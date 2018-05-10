13d4e6353168a86868248176febc58538815543f
Enrique Suárez <ejsuarez793@gmail.com>
2018-05-10 18:57:09 -0300

    3er commit

--- banjampp.py
+++ banjampp.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 import logging
 import pandas as pd
 import numpy as np
@@ -8,9 +9,12 @@ from telegram.ext import CommandHandler
 from telegram.ext import MessageHandler, Filters
 
 TELEGRAM_TOKEN = '521985001:AAFjybE5ZOIlxdSzozxjPa0Gx0lU1EqIzC4'
+MAPS_TOKEN = 'AIzaSyCwkxYSmXosfXb6_YtmbE5USz8OigOHjws'
+MAPS_URL = 'https://maps.googleapis.com/maps/api/staticmap?'
 CSV_FILE = 'https://data.buenosaires.gob.ar/api/files/cajeros-automaticos.csv/download/csv'
 N_ATM = 3
-MAX_DIST = 0.5 #in km
+MIN_ATM_BANK = 1  # min atm number per bank, dataset have some row with 0 atms
+MAX_DIST = 0.5  # in km
 
 
 class TelegramBot(object):
@@ -24,7 +28,6 @@ class TelegramBot(object):
         self.dispatcher.add_handler(CommandHandler('start', self.start))
         self.dispatcher.add_handler(CommandHandler('link', self.link))
         self.dispatcher.add_handler(CommandHandler('banelco', self.banelco))
-        self.dispatcher.add_handler(MessageHandler(Filters.text, self.echo))
         self.dispatcher.add_handler(MessageHandler(Filters.location, self.location))
 
     def start(self, bot, update):
@@ -38,11 +41,37 @@ class TelegramBot(object):
         reply_markup = telegram.ReplyKeyboardMarkup([[keyboard]])
         bot.send_message(chat_id=update.message.chat_id, text="Hola! para enviarte los cajeros de banelco debo solicitar tu ubicación",reply_markup=reply_markup)
 
-    def echo(self, bot, update):
-        bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
-
     def location(self, bot, update):
-        self.atm_locator.lookup(update.message.location.latitude, update.message.location.longitude)
+        user_lat = update.message.location.latitude
+        user_lon = update.message.location.longitude
+        df = self.atm_locator.lookup(user_lat, user_lon)
+        bot.send_message(chat_id=update.message.chat_id, text=self.generate_resp_msg(df))
+        bot.send_message(chat_id=update.message.chat_id, text=self.generate_static_map(user_lat, user_lon, df))
+
+    def generate_resp_msg(self, df):
+        resp_msg = ""
+        i = 1
+
+        msg = "{}) El Banco: {} posee {} teminal(es) de la red {} y se encuentra ubicado en {}\n"
+
+        for index, row in df.iterrows():
+            resp_msg += msg.format(str(i), row['BANCO'], str(row['TERMINALES']), row['RED'], row['DOM_GEO'])
+            i += 1
+        return resp_msg
+
+    def generate_static_map(self, user_lat, user_lon, df):
+        center = '{},{}'.format(str(user_lat), str(user_lon))
+        size = '500x400'
+        zoom = '15'
+        marker = '&markers=color:{}|label:{}|{}'
+
+        params = 'center={}&size={}&zoom={}&key={}'.format(center, size, zoom, MAPS_TOKEN)
+        params += marker.format("blue", "V", center)
+
+        for index, row in df.iterrows():
+            params += marker.format("red", str(i), "{},{}".format(row['LAT'],row['LNG']))
+            i += 1
+        return MAPS_URL + params
 
     def start_bot(self):
         self.updater.start_polling()
@@ -51,20 +80,39 @@ class TelegramBot(object):
 class AtmLocator(object):
 
     def __init__(self):
+        self.df = self.clean_dataset()
+        self.tree = KDTree(np.array(self.df[['LAT', 'LNG']]), leaf_size=2)
+
+    def clean_dataset(self):
         df = pd.read_csv(CSV_FILE, sep=';')
         dfFiltered = df.filter(items=['ID', 'LAT', 'LNG', 'BANCO', 'RED', 'DOM_ORIG', 'DOM_GEO', 'TERMINALES', 'BARRIO', 'COMUNA'])
         dfFilteredNoNan = dfFiltered.dropna(axis=0, how='any')
         dfFilteredNoNan['LAT'] = dfFilteredNoNan['LAT'].apply(lambda s: s.replace(',', '.'))
         dfFilteredNoNan['LNG'] = dfFilteredNoNan['LNG'].apply(lambda s: s.replace(',', '.'))
-        self.df = dfFilteredNoNan
-        #self.df['LAT'] = pd.to_numeric(self.df['LAT'], downcast='float')
-        #self.df['LNG'] = pd.to_numeric(self.df['LNG'], downcast='float')
-        X = np.array(self.df[['LAT', 'LNG']])
-        self.tree = KDTree(X, leaf_size=2)
+        dfFilteredNoNan = dfFilteredNoNan[dfFilteredNoNan['TERMINALES'] >= MIN_ATM_BANK]
+        return dfFilteredNoNan
 
     def lookup(self, user_lat, user_lon):
 
-        def haversine_np(lon1, lat1, lon2, lat2):
+        user_location = np.array([[user_lat, user_lon]])
+
+        dist, ind = self.tree.query(user_location, k=N_ATM)
+        df_temp = pd.DataFrame(self.df, index=ind[0], columns=['ID', 'LAT', 'LNG', 'BANCO', 'RED', 'DOM_GEO', 'TERMINALES', 'BARRIO'])
+
+        df_temp['ORIGIN_LAT'] = user_lat
+        df_temp['ORIGIN_LNG'] = user_lon
+        df_temp['LAT'] = pd.to_numeric(df_temp['LAT'], downcast='float')
+        df_temp['LNG'] = pd.to_numeric(df_temp['LNG'], downcast='float')
+        dest_lon = df_temp['LNG'].as_matrix()
+        dest_lat = df_temp['LAT'].as_matrix()
+        orig_lon = df_temp['ORIGIN_LNG'].as_matrix()
+        orig_lat = df_temp['ORIGIN_LAT'].as_matrix()
+
+        df_temp['DIST'] = self.haversine_np(user_lon, user_lat, dest_lon, dest_lat)
+        df_temp = df_temp[df_temp['DIST'] <= MAX_DIST]
+        return df_temp
+
+    def haversine_np(self, lon1, lat1, lon2, lat2):
             lon1, lat1, lon2, lat2 = map(np.radians, [lon1, lat1, lon2, lat2])
 
             dlon = lon2 - lon1
@@ -76,52 +124,7 @@ class AtmLocator(object):
             km = 6367 * c
             return km
 
-        user_location = np.array([[user_lat, user_lon]])
-
-        dist, ind = self.tree.query(user_location, k=N_ATM)
-        df_temp = pd.DataFrame(self.df, index=ind[0], columns=['LAT', 'LNG', 'BANCO', 'DOM_GEO'])
-        df_temp['ORIGIN_LAT'] = user_lat
-        df_temp['ORIGIN_LNG'] = user_lon
-        df_temp['LAT'] = pd.to_numeric(df_temp['LAT'], downcast='float')
-        df_temp['LNG'] = pd.to_numeric(df_temp['LNG'], downcast='float')
-        dest_lon = df_temp['LNG'].as_matrix()
-        dest_lat = df_temp['LAT'].as_matrix()
-        orig_lon = df_temp['ORIGIN_LNG'].as_matrix()
-        orig_lat = df_temp['ORIGIN_LAT'].as_matrix()
-        """print(type(dest_lon))
-        print(type(dest_lat))
-        print(type(orig_lon))
-        print(type(orig_lat))"""
-        """print(dest_lon)
-        print(dest_lat)
-        print(orig_lon)
-        print(orig_lat)"""
-        df_temp['dist'] = haversine_np(user_lon, user_lat, dest_lon, dest_lat)
-        df_temp = df_temp[df_temp['dist'] <= MAX_DIST]
-        print(df_temp)
-
-
 
 if __name__ == '__main__':
     tb = TelegramBot()
     tb.start_bot()
-
-
-    """df = pd.read_csv('https://data.buenosaires.gob.ar/api/files/cajeros-automaticos.csv/download/csv', sep=';')
-    dfFiltered = df.filter(items=['ID', 'LAT', 'LNG', 'BANCO', 'RED', 'DOM_ORIG', 'DOM_GEO', 'TERMINALES', 'BARRIO', 'COMUNA'])
-    dfFilteredNoNan = dfFiltered.dropna(axis=0, how='any')
-    # print(dfFilteredNoNan.LAT)
-    dfFilteredNoNan['LAT'] = dfFilteredNoNan['LAT'].apply(lambda s: s.replace(',', '.'))
-    dfFilteredNoNan['LNG'] = dfFilteredNoNan['LNG'].apply(lambda s: s.replace(',', '.'))
-    X = np.array(dfFilteredNoNan[['LAT', 'LNG']])
-    user_location = np.array([[-34.598542, -58.428014]])
-    user_location = np.array([[-34.598542, -58.428014]])
-    # print(X)
-    # a = "-23.2312313"
-    # print(float(a))
-    
-    tree = KDTree(X, leaf_size=2)
-    #print(X[0])
-    dist, ind = tree.query(user_location, k=3)
-    print(dist)
-    print(ind)"""
