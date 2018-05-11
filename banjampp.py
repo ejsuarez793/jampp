b7465e15313056da91ad14569b4d59b899fd5e92
Enrique Suárez <ejsuarez793@gmail.com>
2018-05-10 23:28:46 -0300

    3er dia part 2

--- banjampp.py
+++ banjampp.py
@@ -26,8 +26,8 @@ class TelegramBot(object):
         self.atm_locator = AtmLocator()
 
         self.dispatcher.add_handler(CommandHandler('start', self.start))
-        self.dispatcher.add_handler(CommandHandler('link', self.link))
-        self.dispatcher.add_handler(CommandHandler('banelco', self.banelco))
+        self.dispatcher.add_handler(CommandHandler('link', self.command))
+        self.dispatcher.add_handler(CommandHandler('banelco', self.command))
         self.dispatcher.add_handler(MessageHandler(Filters.location, self.location))
 
     def start(self, bot, update):
@@ -36,15 +36,24 @@ class TelegramBot(object):
     def link(self, bot, update):
         bot.send_message(chat_id=update.message.chat_id, text="Listando cajeros link")
 
-    def banelco(self, bot, update):
-        keyboard = telegram.KeyboardButton("Hola! para enviarte los cajeros de banelco debo solicitar tu ubicación",request_contact=None, request_location=True)
+    def command(self, bot, update):
+        atm_type = update.message.text.replace('/', '')
+        text = "Hola! para enviarte los cajeros de la red {} debo solicitar tu ubicación".format(atm_type)
+        keyboard = telegram.KeyboardButton("Oprime para confirmar!", request_contact=None, request_location=True)
         reply_markup = telegram.ReplyKeyboardMarkup([[keyboard]])
-        bot.send_message(chat_id=update.message.chat_id, text="Hola! para enviarte los cajeros de banelco debo solicitar tu ubicación",reply_markup=reply_markup)
+        bot.send_message(chat_id=update.message.chat_id, text=text,
+                         reply_markup=reply_markup)
 
     def location(self, bot, update):
+        atm_types = ['banelco', 'link']
+        atm_type = ""
+        for at in atm_types:
+            if at in update.message.reply_to_message.text:
+                atm_type = at
+                break
         user_lat = update.message.location.latitude
         user_lon = update.message.location.longitude
-        df = self.atm_locator.lookup(user_lat, user_lon)
+        df = self.atm_locator.lookup(user_lat, user_lon, atm_type)
         bot.send_message(chat_id=update.message.chat_id, text=self.generate_resp_msg(df))
         bot.send_message(chat_id=update.message.chat_id, text=self.generate_static_map(user_lat, user_lon, df))
 
@@ -64,12 +73,12 @@ class TelegramBot(object):
         size = '500x400'
         zoom = '15'
         marker = '&markers=color:{}|label:{}|{}'
+        i = 1
 
         params = 'center={}&size={}&zoom={}&key={}'.format(center, size, zoom, MAPS_TOKEN)
         params += marker.format("blue", "V", center)
-
         for index, row in df.iterrows():
-            params += marker.format("red", str(i), "{},{}".format(row['LAT'],row['LNG']))
+            params += marker.format("red", str(i), "{},{}".format(row['LAT'], row['LNG']))
             i += 1
         return MAPS_URL + params
 
@@ -81,7 +90,10 @@ class AtmLocator(object):
 
     def __init__(self):
         self.df = self.clean_dataset()
-        self.tree = KDTree(np.array(self.df[['LAT', 'LNG']]), leaf_size=2)
+        self.dfLink = self.df[self.df['RED'] == "link".upper()]
+        self.dfBanelco = self.df[self.df['RED'] == "banelco".upper()]
+        self.treeBanelco = KDTree(self.dfBanelco[['LAT', 'LNG']], leaf_size=2)
+        self.treeLink = KDTree(self.dfLink[['LAT', 'LNG']], leaf_size=2)
 
     def clean_dataset(self):
         df = pd.read_csv(CSV_FILE, sep=';')
@@ -89,16 +101,26 @@ class AtmLocator(object):
         dfFilteredNoNan = dfFiltered.dropna(axis=0, how='any')
         dfFilteredNoNan['LAT'] = dfFilteredNoNan['LAT'].apply(lambda s: s.replace(',', '.'))
         dfFilteredNoNan['LNG'] = dfFilteredNoNan['LNG'].apply(lambda s: s.replace(',', '.'))
-        dfFilteredNoNan = dfFilteredNoNan[dfFilteredNoNan['TERMINALES'] >= MIN_ATM_BANK]
+        # dfFilteredNoNan = dfFilteredNoNan[dfFilteredNoNan['TERMINALES'] >= MIN_ATM_BANK] #this line remove locations with 0 atm
         return dfFilteredNoNan
 
-    def lookup(self, user_lat, user_lon):
+    def lookup(self, user_lat, user_lon, atm_type):
 
         user_location = np.array([[user_lat, user_lon]])
+        if atm_type == "banelco":
+            print("banelco")
+            dist, ind = self.treeBanelco.query(user_location, k=N_ATM)
+        else:
+            print("link")
+            dist, ind = self.treeLink.query(user_location, k=N_ATM)
 
-        dist, ind = self.tree.query(user_location, k=N_ATM)
-        df_temp = pd.DataFrame(self.df, index=ind[0], columns=['ID', 'LAT', 'LNG', 'BANCO', 'RED', 'DOM_GEO', 'TERMINALES', 'BARRIO'])
-
+        print(ind)
+        print(dist)
+        df_temp = pd.DataFrame(self.dfLink[self.dfLink['RED']], index=ind[0], columns=['ID', 'LAT', 'LNG', 'BANCO', 'RED', 'DOM_GEO', 'TERMINALES', 'BARRIO'])
+        print(df_temp)
+        print("---------------------------------------------------")
+        # df_temp = df_temp[df_temp['RED'] == atm_type.upper()]
+        # print(df_temp)
         df_temp['ORIGIN_LAT'] = user_lat
         df_temp['ORIGIN_LNG'] = user_lon
         df_temp['LAT'] = pd.to_numeric(df_temp['LAT'], downcast='float')
